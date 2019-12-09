d32d59269503c98b38c21121478574aea3f181b0
kirilloid173 <kirilloid173@gmail.com>
2019-12-09 09:50:55 +0400

    0.1

---
+++ main.py
@@ -0,0 +1,16 @@
+name= input("Write you`re name ")
+print("Hello ",name,"!")
+print (type(name))
+
+def kirill():
+    print ("Hello ",name, "!")
+
+def names(name1):
+    print("You`re name is: ",name1)
+
+kirill()
+kirill()
+
+names("Tomas")
+names("Michael")
+input("Press any button for continue")
